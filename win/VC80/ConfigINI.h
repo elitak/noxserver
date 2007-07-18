6202767caba35950b91cb6384e2a69d977bf1aee
mordentral <mordentral@7d00ee9c-f434-0410-81e3-e11b99cc61e3>
2007-07-18 17:39:43 +0000

    Added config.ini file and structure for config.h for the noxserver, this will move the server configuration settings outside of the compilation.
    git-svn-id: https://noxserver.googlecode.com/svn/trunk@39 7d00ee9c-f434-0410-81e3-e11b99cc61e3

---
+++ win/VC80/ConfigINI.h
@@ -0,0 +1,16 @@
+#ifndef H_CONFIG_INI
+#define H_CONFIG_INI
+
+class NXsrvConfig
+{
+private:
+
+public:
+	//port = 18590
+	//UserName = "ZoaBot"
+	//Password = "computer"
+	//Server = "d224.x-mailer.de"
+	//ServerPort = 4000
+};
+
+#endif
